194090d3d507899dc65f5e526357ab2634c9a50d
Andrey S <mailhouse@list.ru>
2024-03-23 13:25:35 +0500

    feat: validate login form; install classnames

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,43 +1,76 @@
-import { useFormik } from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
 
-const renderField = (name, label, type, formik) => {
-  const id = `${name}-form-login`;
+const validationSchema = Yup.object({
+  username: Yup.string()
+    .max(15, 'Must be 15 characters or less')
+    .min(5, 'Must be at least 5 characters')
+    .required('Required'),
+  password: Yup.string()
+    .max(15, 'Must be 15 characters or less')
+    .min(5, 'Must be at least 5 characters')
+    .required('Required'),
+});
+
+const renderFeedback = ({ type, code }) => {
+  const feedbackType = type === 'error' ? 'invalid' : 'valid';
 
   return (
-    <div className="form-group mb-2">
-      <label htmlFor={id} className="text-start">
-        {label}
-        <input
-          id={id}
-          type={type}
-          name={name}
-          className="form-control mt-1"
-          placeholder={label}
-          onChange={formik.handleChange}
-          value={formik.values[name]}
-        />
-      </label>
-
-    </div>
+    <div className={`d-block text-start ${feedbackType}-feedback`}>{code}</div>
   );
 };
 
-const LoginForm = () => {
-  const formik = useFormik({
-    initialValues: { login: '', password: '' },
-    onSubmit: (values) => {
-      console.log(values);
-      console.log('Form was sent');
-    },
-  });
+const renderField = (name, label, type, errors) => (
+  <div className="form-group m-auto mb-2">
+    <Field
+      type={type}
+      name={name}
+      className="form-control mt-1"
+      placeholder={label}
+      label={label}
+    />
+    { errors[name] ? renderFeedback({ type: 'error', code: errors[name] }) : null }
+  </div>
+);
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      {renderField('login', 'Login', 'text', formik)}
-      {renderField('password', 'Password', 'password', formik)}
-      <button type="submit" className="btn btn-primary mt-1">Submit</button>
-    </form>
-  );
-};
+const LoginForm = () => (
+  <Formik
+    initialValues={{ username: '', password: '' }}
+    validationSchema={validationSchema}
+    validateOnBlur={false}
+    validateOnChange={false}
+    onSubmit={async (values, { setStatus }) => {
+      try {
+        const response = await axios.post('/api/v1/login', values);
+        const { data } = response;
+        setStatus({ type: 'success', code: 'Success' });
+        console.log(data);
+      } catch (e) {
+        setStatus({ type: 'error', code: 'login' });
+      }
+    }}
+  >
+    {({ values, errors, status }) => {
+      const isEmpty = Object.values(values).some((v) => v === '');
 
+      return (
+        <Form className="m-auto w-25">
+          {status && renderFeedback(status)}
+
+          {renderField('username', 'Username', 'text', errors)}
+          {renderField('password', 'Password', 'password', errors)}
+
+          <button
+            type="submit"
+            className="btn btn-primary mt-1"
+            disabled={isEmpty}
+          >
+            Submit
+          </button>
+        </Form>
+      );
+    }}
+  </Formik>
+);
 export default LoginForm;
