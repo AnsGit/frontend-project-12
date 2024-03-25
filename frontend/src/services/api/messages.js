2bdfa6be8a16d56f7b4022df43de1740d312a06b
Andrey S <mailhouse@list.ru>
2024-03-25 20:15:47 +0500

    feat: init channels and messages api

---
+++ frontend/src/services/api/messages.js
@@ -0,0 +1,50 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const messagesApi = createApi({
+  reducerPath: 'messages',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/v1/messages',
+    prepareHeaders: ((headers, { getState }) => {
+      const state = getState();
+
+      if (state?.user?.token) {
+        headers.set('Authorization', `Bearer ${state.user.token}`);
+      }
+
+      return headers;
+    }),
+  }),
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: () => '',
+    }),
+    addMessage: builder.mutation({
+      query: (channel) => ({
+        method: 'POST',
+        body: channel,
+      }),
+    }),
+    updateMessage: builder.mutation({
+      query: ({ id, ...body }) => ({
+        url: id,
+        method: 'PATCH',
+        body,
+      }),
+    }),
+    deleteMessage: builder.mutation({
+      query: (id) => ({
+        url: id,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export default messagesApi;
+
+export const {
+  useGetMessagesQuery,
+  useAddMessageMutation,
+  useUpdateMessageMutation,
+  useDeleteMessageMutation,
+} = messagesApi;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
+import channelsApi from '../services/api/channels';
+import messagesApi from '../services/api/messages';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => (
+    getDefaultMiddleware().concat(channelsApi.middleware, messagesApi.middleware)
+  ),
 });
 
 export default store;
