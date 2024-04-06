188e4a5987f2aeaa9bd5b740473a1a62e9652885
Andrey S <mailhouse@list.ru>
2024-04-06 14:41:12 +0500

    fix: add id to forms fields

--- frontend/src/components/forms/components/FormField.jsx
+++ frontend/src/components/forms/components/FormField.jsx
@@ -5,10 +5,11 @@ const FormField = ({
   name, label, type, errors, component = 'input',
 }) => (
   <div className="form-group m-auto mb-2">
-    <label className="visually-hidden" htmlFor={name}>{label}</label>
+    <label className="visually-hidden" htmlFor={`${name}-field`}>{label}</label>
 
     <Field
       type={type}
+      id={`${name}-field`}
       name={name}
       component={component}
       className="form-control mt-1"
